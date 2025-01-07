d78de475f070d54cc9d6d1c5184510e67493f236
fbansept <bansept.franck@gmail.com>
2025-01-07 11:51:54 +0100

    fix video

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,8 +3,6 @@ import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import Feather from "@expo/vector-icons/Feather";
-import { Video, ResizeMode, AVPlaybackStatus } from "expo-av";
-import { useRef, useState } from "react";
 import { useEvent } from "expo";
 import { useVideoPlayer, VideoView } from "expo-video";
 
@@ -20,20 +18,22 @@ export type CardItem = {
 };
 
 export function Card({ item }: { item: CardItem }) {
-  const player: any = useVideoPlayer(
-    "https://videocdn.cdnpk.net/videos/92662fc6-7559-5644-9421-33e37c382bbd/vertical/previews/clear/large.mp4?token=exp=1736244390~hmac=7a4ccdfdae72686e1d07d6f887b5b65b6936e62bc13df97990b4fa7207ec2e85",
-    (player) => {
-      player.loop = true;
-      player.play();
-    }
-  );
+  let content = null;
 
-  const { isPlaying } = useEvent(player, "playingChange", {
-    isPlaying: player.playing
-  });
+  if (item.mediaKind == "movie") {
+    const player: any = useVideoPlayer(
+      "https://cdn.pixabay.com/video/2024/12/03/244754_large.mp4",
+      (player) => {
+        player.loop = true;
+        player.play();
+      }
+    );
 
-  const content =
-    item.mediaKind == "movie" ? (
+    const { isPlaying } = useEvent(player, "playingChange", {
+      isPlaying: player.playing
+    });
+
+    content = (
       <View>
         <VideoView
           style={styles.video}
@@ -54,7 +54,9 @@ export function Card({ item }: { item: CardItem }) {
           />
         </View>
       </View>
-    ) : (
+    );
+  } else {
+    content = (
       <Image
         style={{ height: 500, objectFit: "cover" }}
         source={{
@@ -62,53 +64,56 @@ export function Card({ item }: { item: CardItem }) {
         }}
       ></Image>
     );
+  }
 
-  return (
-    <View style={styles.container}>
-      <View>
-        {content}
-        <View style={[styles.row, styles.actionButtonsContainer]}>
-          <View style={styles.row}>
-            <MaterialCommunityIcons
-              name="heart-outline"
-              size={36}
-              color="black"
-            />
-            <Ionicons name="chatbubble-outline" size={36} color="black" />
-            <Feather name="send" size={36} color="black" />
-          </View>
-          <FontAwesome6 name="bookmark" size={30} color="black" />
-        </View>
-        <Text>{item.like} J'aime</Text>
-      </View>
-      <View
-        style={[
-          styles.header,
-          styles.row,
-          item.mediaKind == "picture" ? styles.headerPicture : null
-        ]}
-      >
-        <View style={styles.row}>
-          <Image
-            style={styles.avatar}
-            source={{ uri: item.author.avatar }}
-          ></Image>
-          <Text>{item.author.pseudo}</Text>
-        </View>
+  if (content) {
+    return (
+      <View style={styles.container}>
         <View>
-          <View
-            style={
-              item.mediaKind == "picture"
-                ? styles.outlineButtonPicture
-                : styles.outlineButtonMovie
-            }
-          >
-            <Text style={styles.outlineButtonText}>Suivre</Text>
+          {content}
+          <View style={[styles.row, styles.actionButtonsContainer]}>
+            <View style={styles.row}>
+              <MaterialCommunityIcons
+                name="heart-outline"
+                size={36}
+                color="black"
+              />
+              <Ionicons name="chatbubble-outline" size={36} color="black" />
+              <Feather name="send" size={36} color="black" />
+            </View>
+            <FontAwesome6 name="bookmark" size={30} color="black" />
+          </View>
+          <Text>{item.like} J'aime</Text>
+        </View>
+        <View
+          style={[
+            styles.header,
+            styles.row,
+            item.mediaKind == "picture" ? styles.headerPicture : null
+          ]}
+        >
+          <View style={styles.row}>
+            <Image
+              style={styles.avatar}
+              source={{ uri: item.author.avatar }}
+            ></Image>
+            <Text>{item.author.pseudo}</Text>
+          </View>
+          <View>
+            <View
+              style={
+                item.mediaKind == "picture"
+                  ? styles.outlineButtonPicture
+                  : styles.outlineButtonMovie
+              }
+            >
+              <Text style={styles.outlineButtonText}>Suivre</Text>
+            </View>
           </View>
         </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
