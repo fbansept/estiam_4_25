782079befd6f9131e87d32e5cfc698cd963d6025
fbansept <bansept.franck@gmail.com>
2025-01-09 09:17:21 +0100

    login

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaContainer } from "@/components/SafeAreaContainer";
+import { StyleSheet, View } from "react-native";
+
+export default function TabTwoScreen() {
+  return <SafeAreaContainer>
+
+    
+
+  </SafeAreaContainer>;
+}
+
+const styles = StyleSheet.create({});
