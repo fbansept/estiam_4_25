431ba895c0bdba8c8c0934ae6290f84a2d1e2470
fbansept <bansept.franck@gmail.com>
2025-01-09 16:57:25 +0100

    login force

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,10 +1,15 @@
 import { SafeAreaContainer } from "@/components/SafeAreaContainer";
 import { Button, Input } from "@rneui/themed";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import { Alert, StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import * as SecureStore from "expo-secure-store";
+import { useNavigation } from "expo-router";
+import { NavigationProp, ParamListBase } from "@react-navigation/native";
 
-export default function TabTwoScreen() {
+export default function Login() {
+  const [jwt, setJwt] = useState<string | null>(null);
+
   const {
     control,
     handleSubmit,
@@ -12,98 +17,118 @@ export default function TabTwoScreen() {
     getValues
   } = useForm();
 
-  function fetchWithTimeout(url: string, options: Object, timeout = 3000) {
-    return Promise.race([
-      fetch(url, options),
-      new Promise((_, reject) =>
-        setTimeout(() => reject(new Error("timeout")), timeout)
-      )
-    ]);
-  }
+  const navigation: NavigationProp<ParamListBase> = useNavigation();
 
-  const onFormValid = (donneesFormulaire: any) => {
-    fetchWithTimeout("http://192.168.43.59:3000/login", {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(donneesFormulaire)
-    })
-      .then((resultat: any) => {
-        if (resultat.status == 403) {
-          Alert.alert("Connexion refusée", "Les identifiants sont incorrects");
-          throw Error("403");
-        }
+  useEffect(() => {
+    setJwt(SecureStore.getItem("jwt"));
+  }, []);
 
-        console.log("toto");
+  if (!jwt) {
+    function fetchWithTimeout(url: string, options: Object, timeout = 3000) {
+      return Promise.race([
+        fetch(url, options),
+        new Promise((_, reject) =>
+          setTimeout(() => reject(new Error("timeout")), timeout)
+        )
+      ]);
+    }
 
-        return resultat.text();
+    const onFormValid = (donneesFormulaire: any) => {
+      fetchWithTimeout("http://192.168.43.59:3000/login", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(donneesFormulaire)
       })
-      .then((jwt) => console.log(jwt))
-      .catch((erreur) => {
-        //l'erreur 403 a déjà été affichée
-        if (erreur.message == "403") {
-          return;
-        }
-
-        Alert.alert(
-          "Connexion impossible",
-          "Impossible de se connecter, vérifiez votre connexion internet"
-        );
-      });
-  };
-
-  return (
-    <SafeAreaContainer>
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={(value: any) => onChange(value)}
-            value={value}
-            label="Email"
-            errorStyle={{ color: "red" }}
-            errorMessage={errors.email ? errors.email.message?.toString() : ""}
-          />
-        )}
-        name="email"
-        rules={{
-          required: {
-            value: true,
-            message: "Ce champs est obligatoire"
-          },
-          pattern: {
-            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-            message: "Veuillez entrer une adresse email valide."
+        .then((resultat: any) => {
+          if (resultat.status == 403) {
+            Alert.alert(
+              "Connexion refusée",
+              "Les identifiants sont incorrects"
+            );
+            throw Error("403");
           }
-        }}
-        defaultValue=""
-      />
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={(value: any) => onChange(value)}
-            value={value}
-            label="Mot de passe"
-            errorStyle={{ color: "red" }}
-            errorMessage={
-              errors.password ? errors.password.message?.toString() : ""
+
+          return resultat.text();
+        })
+        .then(async (jwt) => {
+          await SecureStore.setItemAsync("jwt", jwt);
+          setJwt(jwt);
+        })
+        .catch((erreur) => {
+          //l'erreur 403 a déjà été affichée
+          if (erreur.message == "403") {
+            return;
+          }
+
+          Alert.alert(
+            "Connexion impossible",
+            "Impossible de se connecter, vérifiez votre connexion internet"
+          );
+        });
+    };
+
+    return (
+      <SafeAreaContainer>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={(value: any) => onChange(value)}
+              value={value}
+              label="Email"
+              errorStyle={{ color: "red" }}
+              errorMessage={
+                errors.email ? errors.email.message?.toString() : ""
+              }
+            />
+          )}
+          name="email"
+          rules={{
+            required: {
+              value: true,
+              message: "Ce champs est obligatoire"
+            },
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Veuillez entrer une adresse email valide."
             }
-          />
-        )}
-        name="password"
-        rules={{
-          required: {
-            value: true,
-            message: "Ce champs est obligatoire"
-          }
-        }}
-        defaultValue=""
-      />
-      <Button title="Envoyer" onPress={handleSubmit(onFormValid)} />;
-    </SafeAreaContainer>
-  );
+          }}
+          defaultValue="a@a.com"
+        />
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={(value: any) => onChange(value)}
+              value={value}
+              label="Mot de passe"
+              errorStyle={{ color: "red" }}
+              errorMessage={
+                errors.password ? errors.password.message?.toString() : ""
+              }
+            />
+          )}
+          name="password"
+          rules={{
+            required: {
+              value: true,
+              message: "Ce champs est obligatoire"
+            }
+          }}
+          defaultValue="root"
+        />
+        <Button title="Envoyer" onPress={handleSubmit(onFormValid)} />;
+      </SafeAreaContainer>
+    );
+  } else {
+    return (
+      <SafeAreaContainer>
+        <Text>Donnee de l'utilisateur</Text>
+      </SafeAreaContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({});
